f7b6a0dde78d75268abfdb11769c2117d6bb791e
RoseMarly <rose.marly@unil.ch>
2022-04-13 11:32:59 +0200

    début de template + réorganisation
    fichier séparé pour les fonctions
    début du template game (cassé), qui contiendra tout l'aspect jeu, avec un sous-template card
    
    commentaires des aspects à changer + des éléments à rendre sous template (btn uno)

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+import 'ui/gameFunctions.js'; //FONCTIONS: à importer du fichier gameFunctions.js
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
@@ -8,10 +9,8 @@ Template.hello.onCreated(function helloOnCreated() {
   this.counter = new ReactiveVar(0);
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.game.helpers({
+  players
 });
 
 Template.hello.events({
@@ -23,65 +22,46 @@ Template.hello.events({
 
 //1er cractère: couleur
 //2ème caractère: valeur
-let deck = [
-  "R0", "R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "RS", "RB", "R+2",
-  "B0", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "BS", "BB", "B+2",
-  "V0", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "VS", "VB", "V+2",
-  "JO", "J1", "J2", "J3", "J4", "J5", "J6", "J7", "J8", "J9", "JS", "JB", "J+2",
-  "NC", "NC", "N+4", "N+4",
-  "R0", "R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "RS", "RB", "R+2",
-  "B0", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "BS", "BB", "B+2",
-  "V0", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "VS", "VB", "V+2",
-  "JO", "J1", "J2", "J3", "J4", "J5", "J6", "J7", "J8", "J9", "JS", "JB", "J+2",
-  "NC", "NC", "N+4", "N+4"
-]
-let discard = [] //défausse, se remplira au fil de la partie
-
-let turn = 0     //pour suivre chaque tour du jeu, turn++ après chaque carte jouée, puis turn%joueurs pour suivre l'ordre de jeu
-
-//return n cartes piochées et enlevées du deck
-function draw(n){ 
-  let drawedCards = []
-
-  for(i = 0; i < n; i++){
-    if(!deck.length){recycleDiscard()} //si deck.length != 1 ou plus (donc si = 0, --> correspond à false)
-    
-    let rng = Math.floor((Math.random() * deck.length))
-    drawedCards.push(deck[rng]) //stock la valeur de la carte
-    deck.splice(rng, 1) //retire la carte du deck
-  }
-  return drawedCards
+//à changer: sous forme d'objet (voir ci-dessous) + en faire une collection (base de données)
+// (permettra de décider qu'est ce qui est accessible ou non à qui)
+let data = {
+  "deck": [
+    "R0", "R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "RS", "RB", "R+2",
+    "B0", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "BS", "BB", "B+2",
+    "V0", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "VS", "VB", "V+2",
+    "J0", "J1", "J2", "J3", "J4", "J5", "J6", "J7", "J8", "J9", "JS", "JB", "J+2",
+    "NC", "NC", "N+4", "N+4",
+    "R0", "R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "RS", "RB", "R+2",
+    "B0", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "BS", "BB", "B+2",
+    "V0", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "VS", "VB", "V+2",
+    "J0", "J1", "J2", "J3", "J4", "J5", "J6", "J7", "J8", "J9", "JS", "JB", "J+2",
+    "NC", "NC", "N+4", "N+4"
+    ],
+  "players": [
+    {
+      "id": "uihdoi87g1b3u2i",
+      "hand": [],
+    },
+    {
+      "id": "oh312983h9813h2",
+      "hand": [],
+    },
+    {
+      "id": "983h1293h12983h",
+      "hand": [],
+    },
+    {
+      "id": "eh129e8h12ehkeq",
+      "hand": [],
+    },
+  ]
 }
 
-function recycleDiscard(){
-  deck = [...discard] //copier la discard dans le deck
-  deck.splice(deck.length, 1) //retirer la dernière carte, qui reste dans la discard
-
-  let lastDiscard = discard[discard.length] //sauvegarder la dernière carte
-  discard = [lastDiscard] // vider la discard
-}
-
-function setup(players, cards){
-  for(i = 0; i < players; i++){
-    let playerHand = [] //créer une main par "players" (il faudra renommer la variable selon i)
-    for(j = 0; j < cards; j++){
-      let pioche = draw() // lui donner une carte par "cards"
-      playerHand.push(pioche)
-    }
-  }
-}
-
-//player: joueur qui essaie de jouer, pour vérifier si il peut jouer
-// card: la carte qu'il essaie de jouer
-function play(player, card){  
-
-discard.push()
-}
 
 let BoutonUno = document.getElementById("btnUNO")
 BoutonUno.addEventListener("click",function dernierCarte(){
   if (playerHand == 1){
-    BoutonUno.addEventListener("click",function dernierCarte(){
+    BoutonUno.addEventListener("click",function dernierCarte(){ //implémenter l'interactivité par template
       alert("UNO !!")
     })
   }})
